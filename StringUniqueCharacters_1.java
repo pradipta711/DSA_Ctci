257b7aaf75935bb9f9a8c516dfa776e8e1030791
Pradipta Pariyal <33295657+pradipta711@users.noreply.github.com>
2022-01-31 23:05:20 -0600

    Day #1 Array&String

---
+++ StringUniqueCharacters_1.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class StringUniqueCharacters_1 {
+
+
+    public static void main(String args[]) {
+        StringUniqueCharacters_1 s1 = new StringUniqueCharacters_1();
+        boolean res = s1.isUnique_BF("tyurrrrdf");
+        boolean res2 = s1.isUnique_Set("fghjkjhg");
+        System.out.println("String is......." + res);
+        System.out.println("String is......." + res2);
+
+
+    }
+
+    public boolean isUnique_BF(String s) {      //O(N^2)
+        for (int i = 0; i < s.length() - 1; i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean isUnique_Set(String s1) {
+        Set<Character> findChars = new HashSet<>(); {
+            for(char c: s1.toCharArray())
+            {
+                if(findChars.contains(c))
+                {
+                    return false;
+                }
+                findChars.add(c);
+            }
+          return true;
+
+        }
+
+
+    }
+}
